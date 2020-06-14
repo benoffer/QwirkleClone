a6bfda0a3b739ae25d8d86d61bf9d9f60a5dc50c
Benjamin Offer <ben.offer@gmail.com>
2020-06-14 16:21:53 -0400

    Pushing Working Starting Shuffle

--- QwirkleClone/src/main/java/com/techelevator/GamePieces.java
+++ QwirkleClone/src/main/java/com/techelevator/GamePieces.java
@@ -2,41 +2,44 @@ package com.techelevator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Stack;
 
 public class GamePieces {
 
-	private ArrayList<String> gamePieces;
-	private ArrayList<String> player1Hand;
-	private ArrayList<String> player2Hand;
+	private Stack<String> gamePieces = new Stack<>();
+	private ArrayList<String> player1Hand = new ArrayList<>();
+	private ArrayList<String> player2Hand = new ArrayList<>();
 
 	private String[] colors = new String[] { "Blue", "Yellow", "Green", "Purple", "Orange", "White" };
 	private String[] shapes = new String[] { "Star", "Circle", "Triangle", "Square", "Diamond", "Horseshoe" };
 
 	public GamePieces() {
-		ArrayList<String> gamePieces = new ArrayList<>();
+
+		Stack<String> gamePieces = new Stack<>();
 
 		for (int c = 0; c < colors.length; c++) {
 			for (int s = 0; s < colors.length; s++) {
 				for (int n = 0; n < 3; n++) {
-					gamePieces.add(colors[c] + "," + shapes[s]);
+					gamePieces.push(colors[c] + "|" + shapes[s]);
 				}
 			}
 		}
 		this.gamePieces = gamePieces;
+		Collections.shuffle(gamePieces);
 	}
 	
 	public void startingDeal() {
-		Collections.shuffle(gamePieces);
+
 		for (int i = 0; i < 6; i++) {
-			player1Hand.add(gamePieces.get(0));
-			player2Hand.add(gamePieces.get(0));
+			player1Hand.add(gamePieces.pop());
+			player2Hand.add(gamePieces.pop());
 		}
 		System.out.println(player1Hand);
 		System.out.println(player2Hand);
 	}
 
 
-	public ArrayList<String> getGamePieces() {
+	public Stack<String> getGamePieces() {
 		return gamePieces;
 	}
 
